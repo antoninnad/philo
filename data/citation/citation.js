93955cd61b5f6dfde1ed25e05995193ef3c15b1b
anto.nadaud <nadaudantonin@gmail.com>
2022-11-16 14:19:23 +0100

    first commit

---
+++ data/citation/citation.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        "name": "Galilé Italien XII",
+        "contain": "Le grand livre du monde a été écrit en langauge mathématique"
+    },
+    {
+        "name": "Auguste Conte",
+        "contain": "Science d'où prévoyance, prévoyance d'où action \n On peut prendre l'exemple de la fille qui a reconnu des signes d'un tsunami"
+    },
+    {
+        "name": "Emil Michel Cioran",
+        "contain": "En permettant l’homme, la nature a commis beaucoup plus qu’une erreur de calcul : un attentat contre elle-même."
+    }, 
+    {   
+        "name": "Pascal",
+        "contain": "La vie de l'homme est missérablement courte"
+    }, {
+        "name": "Platon",
+        "contain": "La pensée c'est le dialogue intérieur et silencieux de l'âme avec elle-même (version longue). Version courte: penser c'est parler à soi"
+    }, {
+        "name": "Sophocle",
+        "contain": "Entre tant de merveille du monde la grande merveille c'est l'homme."
+    }, {
+        "name": "La fontaine",
+        "contain": "En toute chose il faut considérer la fin. Ce proverbe signifie que c'est la manière dont se termine une chose qui est importante."
+    } , {
+        "name": "Kant",
+        "contain": "L'homme est un animal qui a besoin d'un maître."
+    } , {
+        "name": "On parle de bon sauvage",
+        "Contain": "l'homme est naturelement bon."
+    }, {
+        "name": "Montaigne",
+        "contain": "Chacun juge de barbare ce qui n'est pas de son usage."
+    } , {
+        "name": "Lévi straus",
+        "contain": "Le barbare c'est l'homme qui croit à la barbarie."
+    } , {
+        "name": "Heidegger",
+        "contain": "La nature devient objet pour lequel on prend commande."
+    } 
+];
+
+export data;
